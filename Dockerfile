644c6cf7ad5e3641abc1da332477cc3b29d5892f
yuparan <jessea11@naver.com>
2022-10-10 11:45:00 +0900

    docker image 상의 MySQL 포트를 3306 으로 고정 > 포트포워딩 으로 외부 포트 변경 방식 채택

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 # ARG Env-Parameter:
 ARG MYSQL_VERSION=8.0.30
-ARG MYSQL_PORT=3306
 
 # Base Image
 FROM mysql:${MYSQL_VERSION}
 
 # Open Port for the Python App
-EXPOSE ${MYSQL_PORT}
+EXPOSE 3306
--- bin/build_docker_image.sh
+++ bin/build_docker_image.sh
@@ -10,5 +10,4 @@ source $PROJECT_PATH/bin/loadenv.sh
 # Cross-platform CPU(M1 Mac) 대응을 위해 `--platform linux/amd64` 가 필요 합니다
 docker build --platform linux/amd64 \
     --build-arg MYSQL_VERSION=$MYSQL_VERSION \
-    --build-arg MYSQL_PORT=$MYSQL_PORT \
     -t $PROJECT_DATABASE_NAME:mysql-$MYSQL_VERSION .
